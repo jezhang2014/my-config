4ddfa4fc74a6cf33e91aafb5f9746201b61dae04
root <root@docker.192.168.1.1>
2022-07-03 12:46:22 +0000

    add anaconda

---
+++ docker/anaconda/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t anaconda3:my-conda .
+
---
+++ docker/anaconda/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker compose up -d
+
+#conda update -n base -c defaults conda
+#conda install xgboost
+#conda update --all
+#apt update
+#apt upgrade
